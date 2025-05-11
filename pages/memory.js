a7cdfa72b531ee1b71be6ec8bedd6bddeec91ae8
gladiy <alisaepikova@yandex.ru>
2025-05-11 14:59:51 +0300

    Fix wrong path to MemoryGame

--- pages/memory.js
+++ pages/memory.js
@@ -2,7 +2,7 @@
 import dynamic from 'next/dynamic';
 
 // Включаем динамическую загрузку, чтобы избежать проблем с SSR
-const MemoryGame = dynamic(() => import('../../components/MemoryGame'), { ssr: false });
+const MemoryGame = dynamic(() => import('../components/MemoryGame'), { ssr: false });
 
 export default function MemoryPage() {
   return <MemoryGame />;
